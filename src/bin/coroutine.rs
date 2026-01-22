f10050e5f381eb7ebe0222a5c2c59a6580f8b5e0
Sam Liu <ontouchstart@gmail.com>
2026-01-21 21:35:24 -0500

    update src/bin/coroutine.rs

--- src/bin/coroutine.rs
+++ src/bin/coroutine.rs
@@ -1,7 +1,10 @@
 //! <https://wtfm-rs.github.io/nightly/rust/html/unstable-book/index.html>
+//!
 //! This only works in nightly
 //! ```sh
-//! rustup run nightly cargo  run --bin coroutine
+//! % rustup run nightly rustc src/bin/coroutine.rs && ./coroutine && rm ./coroutine
+//! It works!
+//! % rustup run nightly cargo  run --bin coroutine
 //!   Compiling wtfm_nightly v0.1.0 (/Users/sam/github/wtfm-rs/nightly)
 //! error[E0554]: `#![feature]` may not be used on the stable release channel
 //!  --> src/bin/coroutine.rs:1:1
